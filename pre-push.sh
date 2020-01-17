fc9c4761cd5dc603161b1972868589d1c067f80d
vishmango117 <vishal-lollers@hotmail.com>
2020-01-18 04:15:15 +0800

    Final Deployment Changes

--- pre-push.sh
+++ pre-push.sh
@@ -1,6 +1,6 @@
 # Obtain SQL Dump from the Amazon RDS (Local Sync)
 rm doctorsconnectdb.sql
-mysqldump -ha2team11-stagingdb.co3jt0kipxzh.ap-southeast-1.rds.amazonaws.com -uadmin -pmalaysia_boleh doctorsconnectdb > doctorsconnectdb.sql
+mysqldump -ha2team11-stagingdb.co3jt0kipxzh.ap-southeast-1.rds.amazonaws.com -uadmin -pmalaysia_boleh doctorsconnectdb > my-defaults/doctorsconnect.sql
 # shift the .sql file to the database directory
 git add .
 git commit -m "$1"
---
+++ wp-content/themes/doctors-test/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package doctors_test
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'doctors-test' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'doctors-test' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'doctors-test' ), 'doctors-test', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

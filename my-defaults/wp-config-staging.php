55c326859c7f9ae57cf8c7643d026cd5678951cf
Vishal Manghnani <vishal-lollers@hotmail.com>
2020-01-18 01:53:24 +0800

    Recreated a new db

--- my-defaults/wp-config-staging.php
+++ my-defaults/wp-config-staging.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'vishalm4_wpstg' );
+define( 'DB_NAME', 'vishalm4_wpstage' );
 
 /** MySQL database username */
-define( 'DB_USER', 'vishalm4_wpstg');
+define( 'DB_USER', 'vishalm4_wpstage');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'vishal117257' );
+define( 'DB_PASSWORD', 'vish117257' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );

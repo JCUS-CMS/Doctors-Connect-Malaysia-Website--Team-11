01c6d2064287cd41b80a56c9ffd3988846265279
vishmango117 <vishal-lollers@hotmail.com>
2020-01-18 04:21:17 +0800

    Final Deployment Changes3

--- test_script.sh
+++ test_script.sh
@@ -21,7 +21,7 @@ echo "${NO_COLOR}"
 
 #Importing Database alongisde the wp files
 echo "${RED}"
-mysql -uvishalm4_wpstage -pvish117257 vishalm4_wp781 < doctorsconnect.sql
+mysql -uvishalm4_wpstage -pvish117257 vishalm4_wp781 < my-defaults/doctorsconnect.sql
 echo "${GREEN}"
 echo "Database Import Complete" >> log_$timestamp.txt
 
